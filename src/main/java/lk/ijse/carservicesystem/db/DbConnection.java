06518f72332e472d64d7b568dd5a2c0d5d8a101a
Your Name <you@example.com>
2023-12-31 21:17:05 +0530

    initial commit

--- src/main/java/lk/ijse/carservicesystem/db/DbConnection.java
+++ src/main/java/lk/ijse/carservicesystem/db/DbConnection.java
@@ -1,9 +1,13 @@
 package lk.ijse.carservicesystem.db;
 
+import lombok.Getter;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+@Getter
 public class DbConnection {
 
     private static DbConnection dbConnection;
@@ -17,8 +21,5 @@ public class DbConnection {
     }
     public static DbConnection getInstance() throws SQLException {
         return (null == dbConnection) ? dbConnection = new DbConnection() : dbConnection;
-    }
-    public Connection getConnection() {
-        return connection;
     }
 }
